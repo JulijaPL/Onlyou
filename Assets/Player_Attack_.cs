19695d5849c0c31388b84586579251bd3b229dc6
Julia <teamspeakjulia@gmail.com>
2026-08-18 21:46:44 +0200

    Added animation

---
+++ Assets/Player_Attack_.cs
@@ -0,0 +1,34 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Player_Attack_ : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    private bool isAnimating = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !isAnimating)
+        {
+            StartAnimation();
+        }
+
+      
+    }
+    void StartAnimation()
+        {
+            isAnimating = true;
+
+        animator.SetTrigger("Attack");
+    }
+
+        public void AnimationFinished()
+        {
+            isAnimating = false;
+        }
+}
